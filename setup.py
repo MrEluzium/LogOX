9eed701b82bf00442795d8dc4db7227d4bbffe40
Artem Eluzium <mreluzium@mail.ru>
2022-01-03 23:51:10 +0500

    Add lecinse and classifiers information to setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,19 @@ with open("README.md", "r") as readme:
 
 setup(
     name='LogOX',
-    version='1.0.0',
+    version='1.0.0b1',
     author="Artem Eluzium",
     description=" My interlayer package for python logging",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MrEluzium/LogOX",
+    license='Apache License 2.0',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: System :: Logging",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
     packages=find_packages(include=['LogOX', 'LogOX.*'])
 )
